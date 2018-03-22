2312e8561badb03a8aa949e193a985e25a00dc23
Alexis Berry <alexisberry@alexiss-mbp-2.attlocal.net>
2018-03-21 21:49:29 -0400

    add custom event listeners

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var h1 = document.getElementById('h1');
+h1.addEventListener('click', function()) {
+  h1.className = 'blue';
+});
+//listens for clicks, turns blue when hears clicks
+
+var aside = document.getElementById('aside');
+aside.addEventListener('click', function()) {
+  aside.innerHTML='You clicked on the aside!'
+});
+//listens for clicks, says 'You clicked on the aside!'
+
+var figure = document.getElementById('figure');
+figure.addEventListener('click', function()) {
+figure.innerHTML='You clicked on the figure!'
+});
+//listens for clicks, says 'You clicked on the figure!'
