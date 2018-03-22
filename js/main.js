979404dfc1061585baf3b6e39edff0a474ecd349
Alexis Berry <alexisberry@alexiss-mbp-2.attlocal.net>
2018-03-21 21:00:41 -0400

    add form and listener

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var input.submit = document.getElementById('submit');
+
+input.submit.addEventListener("click", function()) {
+  console.log("The firstName is: [0]");
+  console.log("The lastName is: [1]");
+  console.log("The email is: [2]");
+  console.log("The message is: [3]");
+}); 
